98ba27fe213ab5e04dec1b503d5d54d925d59a60
Elena Kulikova <elena.k@customgamesstudio.com>
2023-02-28 17:31:18 +0100

    Added CameraTransform

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,18 @@ using TMPro;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float Speed;
+    [SerializeField] float RotationSpeed;
+    //private Rigidbody _rigidbody;
+
+    //[SerializeField] GameObject _camera;
+    //private float _forwardForce;
+    //float _cameraTransform;
+    [SerializeField] float _speedSercetivity;
+    [SerializeField] Transform _cameraTransform;
+    float _yOffset;
+    float _xOffset;
+
     public int Health;
     [SerializeField] GameObject _lossText;
     TextMeshProUGUI _healthText;
@@ -20,8 +32,13 @@ public class Player : MonoBehaviour
         _healthText = FindAnyObjectByType<HealthText>().GetComponent<TextMeshProUGUI>();
         
     }
+    private void Awake()
+    {
+     //   _rigidbody = GetComponent<Rigidbody>();
+    }
     private void Update()
     {
+        
         _healthText.text = "Жизни: " + Health;
         if (Health <= 0)
         {
@@ -31,5 +48,20 @@ public class Player : MonoBehaviour
             _inactiveHealthText.SetActive(false);
 
         }
+    }
+    private void FixedUpdate()
+    {
+        /*
+         * float sideForce = Input.GetAxis("Horizontal") * Speed;
+        _forwardForce = Input.GetAxis("Mouse Y") * _speedSercetivity;
+        _rigidbody.AddForce(sideForce, _forwardForce, 0f);
+        _rigidbody.AddTorque(0f, 0f, 0f);
+        */
+        _yOffset -= Input.GetAxis("Mouse Y") *_speedSercetivity;
+        _yOffset = Mathf.Clamp(_yOffset, -2f, 2.5f);
+        _xOffset -= Input.GetAxis("Mouse X") *_speedSercetivity;
+        _xOffset = Mathf.Clamp(_xOffset, -38f, 38f);
+        _cameraTransform.position = new Vector3(_xOffset, _yOffset, -30);
+
     }
 }
